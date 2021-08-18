c307f087e807c73b88bde406e981a8dc383494bc
ndqc <hyouka266@gmail.com>
2021-08-18 22:18:25 +0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+use std::fs;
+use std::error::Error;
+use std::env;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+
+    let lines = if config.case_sensitive {
+        search_case_sensitive(&config.query, &content)
+    } else {
+        search_case_insensitive(&config.query, &content)
+    };
+
+    for line in lines {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_SENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
+    }
+}
+
+pub fn search_case_insensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    
+    results
+}
+
+pub fn search_case_sensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive,
+end";
+
+        assert_eq!(vec!["safe, fast, productive,"], search(query, content));
+    }
+}
